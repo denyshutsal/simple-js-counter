c2f5294c98cda7376037be2b77c3e53ed32e795e
Denys Hutsal <denyshutsal@gmail.com>
2023-01-05 15:18:20 +0100

    feat: implement save button logic

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 let countEl = document.getElementById("count");
+let logsEl = document.getElementById("logs");
 let count = 0;
+let logs = [];
 
 function increment() {
   count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  logs.push(count);
+  logsEl.textContent = 'Previous entries: ' + logs;
+  count = 0;
   countEl.textContent = count;
 }
